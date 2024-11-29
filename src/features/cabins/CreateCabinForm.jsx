e2774db2357cb030395ac33014a9c1f153e8dcae
ashishkunthe <ashishkunthe2003@gmail.com>
2024-11-29 20:45:21 +0530

    random changes

--- src/features/cabins/CreateCabinForm.jsx
+++ src/features/cabins/CreateCabinForm.jsx
@@ -64,7 +64,7 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    mutate(data);
+    mutate({ ...data, image: data.image[0] });
   }
 
   function onError(errors) {
@@ -136,7 +136,13 @@ function CreateCabinForm() {
 
       <FormRow>
         <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+        <FileInput
+          id="image"
+          accept="image/*"
+          {...register("image", {
+            required: "this field is required",
+          })}
+        />
       </FormRow>
 
       <FormRow>
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = "https://efhjakexvwguprchetth.supabase.co";
+export const supabaseUrl = "https://efhjakexvwguprchetth.supabase.co";
 const supabaseKey =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVmaGpha2V4dndndXByY2hldHRoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzEyODEzMjQsImV4cCI6MjA0Njg1NzMyNH0.O6Gh5X1SIJUlJon6KtNovI7Fsr9m5oLAe90cc9YEZLo";
 const supabase = createClient(supabaseUrl, supabaseKey);
